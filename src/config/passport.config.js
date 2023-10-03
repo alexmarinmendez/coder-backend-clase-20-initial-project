7844767e9a25053ea80f41e4e300f67ca9b6bc08
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-10-02 20:21:24 -0500

    feat: add passport-local for login

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,6 +1,6 @@
 import passport from "passport"
 import local from 'passport-local'
-import { createHash } from "../utils.js"
+import { createHash, isValidPassword } from "../utils.js"
 import UserModel from "../dao/models/user.model.js"
 
 const localStrategy = local.Strategy
@@ -28,7 +28,18 @@ const initializePassport = () => {
         }
     }))
 
-    // passport.use('login')
+    passport.use('login', new localStrategy({
+        usernameField: 'email',
+    }, async(username, password, done) => {
+        try {
+            const user = await UserModel.findOne({ email: username })
+            if (!user) {
+                return done(null, false)
+            }
+            if (!isValidPassword(user, password)) return done(null, false)
+            return done(null, user)
+        } catch(err) {}
+    }))
 
     passport.serializeUser((user, done) => {
         done(null, user._id)
